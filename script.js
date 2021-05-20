557ebcd9b89e2eb6aa0e1b9e1a272cee2edebc8c
JawalArcilla <>
2021-05-20 02:12:47 +0000

    Fixing errors #1

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 document.getElementById('button').addEventListener('click', age)
 
-let number = 5
+const number = 5
 let guess = 0
 
 function age () {
   guess = document.getElementById('button').value
   guess = parseInt(guess)
 
-  while (guess != number){
+  while (guess !== number) {
     guess = prompt('Guess a number between 1 and 10')
 
     if (guess >= 6) {
-    document.getElementById('answer').innerHTML = 'You guessed too high.'
-  } else if (guess <= 4) {
-    document.getElementById('answer').innerHTML = 'You guessed too low.'
-  } else {
-    document.getElementById('answer').innerHTML = 'You guessed the right number!'
+      document.getElementById('answer').innerHTML = 'You guessed too high.'
+  }   else if (guess <= 4) {
+      document.getElementById('answer').innerHTML = 'You guessed too low.'
+  }   else {
+      document.getElementById('answer').innerHTML = 'You guessed the right number!'
+    }
   }
-}
 }
